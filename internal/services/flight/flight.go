103da1f06dec8cdf2a2153c3820382047d6b72a9
nikita <nikitakerzhik@gmail.com>
2024-12-27 12:31:19 +1000

    implemented flight

--- internal/services/flight/flight.go
+++ internal/services/flight/flight.go
@@ -1,12 +1,16 @@
 package flight
 
 import (
+	"fmt"
 	"kursachDB/internal/domain/models"
 	"kursachDB/internal/services"
+	"kursachDB/pkg/mapper"
 	"log/slog"
 )
 
-type Repo interface{}
+type Repo interface {
+	Add(flight models.Flight) error
+}
 
 type Flight struct {
 	log  *slog.Logger
@@ -21,7 +25,26 @@ func New(log *slog.Logger, repo Repo) *Flight {
 }
 
 func (s *Flight) Add(flight services.AddFlight) error {
-	panic("implement me")
+	const op = "Flight.Add"
+	log := s.log.With(
+		slog.String("op", op),
+	)
+
+	fl, err := mapper.FlightAddToFlight(flight)
+	if err != nil {
+		log.Warn(err.Error())
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("starting adding flight")
+	err = s.repo.Add(fl)
+	if err != nil {
+		log.Warn(err.Error())
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	log.Info("finishing adding flight")
+
+	return nil
 }
 
 func (s *Flight) Delete(id int) error {
--- pkg/mapper/mapper.go
+++ pkg/mapper/mapper.go
@@ -1,8 +1,10 @@
 package mapper
 
 import (
+	"fmt"
 	"kursachDB/internal/domain/models"
 	"kursachDB/internal/services"
+	"time"
 )
 
 func PlaceAddToPlace(place services.AddPlace) models.Place {
@@ -12,4 +14,29 @@ func PlaceAddToPlace(place services.AddPlace) models.Place {
 			Type: place.Type,
 		},
 	}
+}
+
+func FlightAddToFlight(flight services.AddFlight) (models.Flight, error) {
+	departure, err := time.Parse("02.01.2006", flight.Departure)
+	if err != nil {
+		return models.Flight{}, fmt.Errorf("wrong departure")
+	}
+
+	arrival, err := time.Parse("02.01.2006", flight.Arrival)
+	if err != nil {
+		return models.Flight{}, fmt.Errorf("wrong arrival")
+	}
+	return models.Flight{
+		From: models.Place{
+			NamePlace: flight.From,
+		},
+		To: models.Place{
+			NamePlace: flight.To,
+		},
+		Departure: departure,
+		Arrival:   arrival,
+		Bus: models.Bus{
+			StateNumber: flight.StateNumber,
+		},
+	}, nil
 }
